5bfdb331ca99c595940b612872160c30ff702022
Baretta911 <133887621+Baretta911@users.noreply.github.com>
2025-05-20 18:09:04 +0700

    Update notesRoutes.js

--- notes-backend/routes/notesRoutes.js
+++ notes-backend/routes/notesRoutes.js
@@ -1,53 +1,19 @@
-const express = require('express');
-const Note = require('../models/note');
+import express from "express";
+import {
+  createNote,
+  deleteNote,
+  getNotes,
+  updateNote,
+  getNoteById,
+} from "../controller/notesController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-// Ambil semua catatan
-router.get('/', async (req, res) => {
-  try {
-    const notes = await Note.findAll();
-    res.json(notes);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+router.get("/notes", verifyToken, getNotes);
+router.get("/notes/:id", verifyToken, getNoteById);
+router.post("/create-notes", verifyToken, createNote);
+router.put("/update-notes/:id", verifyToken, updateNote);
+router.delete("/delete-notes/:id", verifyToken, deleteNote);
 
-// Tambah catatan baru
-router.post('/', async (req, res) => {
-  try {
-    const { title, content } = req.body;
-    if (!title || !content) {
-      return res.status(400).json({ error: "Title dan content wajib diisi" });
-    }
-    const note = await Note.create({ title, content });
-    res.status(201).json(note); // kirim seluruh data note
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// Update catatan
-router.put('/:id', async (req, res) => {
-  try {
-    const { title, content } = req.body;
-    const { id } = req.params;
-    await Note.update({ title, content }, { where: { id } });
-    res.json({ message: 'Catatan diperbarui' });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-// Hapus catatan
-router.delete('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    await Note.destroy({ where: { id } });
-    res.json({ message: 'Catatan dihapus' });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
-
-module.exports = router;
+export default router;
