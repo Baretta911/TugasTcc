167bc866d360090c7096ec8e481369a7a41b3f9f
Baretta911 <133887621+Baretta911@users.noreply.github.com>
2025-05-20 18:05:06 +0700

    Update database.js

--- notes-backend/config/database.js
+++ notes-backend/config/database.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+import{Sequelize} from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
 });
 
-module.exports = sequelize;
+export default db;
